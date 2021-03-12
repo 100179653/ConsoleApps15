36a7aabdbbc93e6a89872875cf7efeb4797a8b8f
100179653 <71873341+100179653@users.noreply.github.com>
2021-03-12 09:33:13 +0000

    App02 w/o comments and console helper Complete

--- ConsoleAppProject/App02/BMI.cs
+++ ConsoleAppProject/App02/BMI.cs
@@ -1,12 +1,137 @@
-﻿namespace ConsoleAppProject.App02
+﻿using System;
+namespace ConsoleAppProject.App02
 {
     /// <summary>
     /// Please describe the main features of this App
     /// </summary>
     /// <author>
-    /// Student Name version 0.1
+    /// Chris Edgley
     /// </author>
     public class BMI
     {
+        //comment here     
+        public const double UNDERWEIGHT = 18.50;
+        public const double NORMAL = 24.99;
+        public const double OVERWEIGHT= 29.99;
+        public const double OBESEI = 34.99;
+        public const double OBESEII = 39.99;
+        public const double OBESEIII = 40.00;
+        
+
+        //comments here
+        public const int PoundsToStones = 14;
+        public const int InchesToFeet = 12;
+
+
+        private double height;
+        private double weight;
+
+        public double BMIIndex = 0.00;
+
+        public void CalcBMIIndex()
+        {
+            OutputHeader();
+            
+            int Unit = GetUnits();
+            if (Unit == 1)
+            {
+                GetMetres();
+                GetKG();
+                CalculateMetricBMI();
+            } 
+            else if (Unit == 2)
+            {
+                GetInches();
+                GetPounds();
+                CalculateImperialBMI();
+            }
+            
+            OutputHealthMessage(BMIIndex);
+            OutputBAME();
+        }
+
+        public void OutputHeader()
+        {
+            Console.WriteLine("\n--------------------------------");
+            Console.WriteLine("-  BMI Calculator  -");
+            Console.WriteLine("-        By       -");
+            Console.WriteLine("--------------------------------\n");
+        }
+
+        private int GetUnits()
+        {
+            int unit = 0;
+            Console.WriteLine("Enter Metric or Imperial : ");
+            string value = Console.ReadLine();
+            if (String.Equals(value, "metric")) unit = 1;
+            else if (String.Equals(value, "imperial")) unit = 2;
+            return unit;
+        }
+
+        private void GetMetres()
+        {
+            Console.Write("Please enter your height in metres : ");
+            String value = Console.ReadLine();
+            height = Convert.ToDouble(value);
+        }
+
+        private void GetKG()
+        {
+            Console.Write("Please enter your weight in kg: ");
+            String value = Console.ReadLine();
+            weight = Convert.ToDouble(value);
+        }
+
+        private void CalculateMetricBMI()
+        {
+            BMIIndex = weight / (height * height);
+        }
+
+        private void OutputHealthMessage(double BMIIndex)
+        {
+            Console.Write("Your BMI is:");
+            Console.WriteLine(BMIIndex);
+            if (BMIIndex < 18.50) Console.WriteLine("You are underweight");
+            else if (BMIIndex < 25) Console.WriteLine("You are of a normal weight!");
+            else if (BMIIndex < 30) Console.WriteLine("You are overweight");
+            else if (BMIIndex < 35) Console.WriteLine("You are obese");
+            else if (BMIIndex < 40) Console.WriteLine("You are morbidly obese");
+            else Console.WriteLine("Consult a Doctor Straight Away!");
+
+        }
+
+        private void GetInches()
+        {
+            Console.Write("Please enter your height in inches : ");
+            String value = Console.ReadLine();
+            height = Convert.ToDouble(value);
+        }
+
+        private void GetPounds()
+        {
+            Console.Write("Please enter your weight in pounds: ");
+            String value = Console.ReadLine();
+            weight = Convert.ToDouble(value);
+        }
+
+        private void CalculateImperialBMI()
+        {
+            BMIIndex = Math.Floor((double) weight / (height * height) * 703 );
+            // BMIIndex = weight * 703 / (height * height);
+        }
+
+        public void OutputBAME()
+        {
+            Console.Write("You have selected that you ");
+            Console.WriteLine("are of a higher risk group.");
+            Console.Write("This means that this BMI test ");
+            Console.WriteLine("may not be suitable for you.");
+            Console.Write("Please consult your Doctor for ");
+            Console.WriteLine("a BMI test based upon your circumstances.");
+        }
+
+        
+            
     }
 }
+
--- ConsoleAppProject/Program.cs
+++ ConsoleAppProject/Program.cs
@@ -11,28 +11,48 @@ namespace ConsoleAppProject
     /// to start Apps 01 to 05 for CO453 CW1
     /// 
     /// This Project has been modified by:
-    /// Derek Peacock 14/12/2020
+    /// Chris Edgley 05/02/2020
     /// </summary>
     public static class Program
     {
+        private static DistanceConverter converter = new DistanceConverter();
+        private static BMI index = new BMI();
+
         public static void Main(string[] args)
         {
-            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.White;
             
             Console.WriteLine("BNU CO453 Applications Programming 2020-2021!");
             Console.WriteLine();
 
             // Using an extension method for each enumeration
-            Console.WriteLine("Using MyEnum Extension Method!\n");
-            Console.WriteLine("MyEnum Value = " + MyEnum.FirstValue);
-            Console.WriteLine("MyEnum Friendly Value = " + MyEnum.FirstValue.EnumValue());
-            Console.WriteLine();
+            // Console.WriteLine("Using MyEnum Extension Method!\n");
+            // Console.WriteLine("MyEnum Value = " + MyEnum.FirstValue);
+            // Console.WriteLine("MyEnum Friendly Value = " + MyEnum.FirstValue.EnumValue());
+            // Console.WriteLine();
 
             // Using an extension method for any enumeration
             //StudentGrades grades = new StudentGrades();
             //grades.TestGradesEnumeration();
-            DistanceConverter converter = new DistanceConverter();
-            converter.ConvertDistance();
+            Console.WriteLine("Options for the user");
+            Console.WriteLine("Created by Chris Edgley");
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("1. Distance Converter");
+            Console.WriteLine("2. BMI Index");
+
+
+            string options = Console.ReadLine();
+
+            if (options == "1")
+            {
+                converter.ConvertDistance();
+            }
+
+            else if (options == "2")
+            {
+                index.CalcBMIIndex();
+            }
+
         }
     }
 }
