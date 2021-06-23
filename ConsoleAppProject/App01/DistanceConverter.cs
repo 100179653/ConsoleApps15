0eff04e1552d13cf4e5545160e590f3ddd95b069
100179653 <71873341+100179653@users.noreply.github.com>
2021-06-23 09:56:10 +0100

    Addition of app01 Black Box Testing

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -2,24 +2,31 @@
 namespace ConsoleAppProject.App01
 {
     /// <summary>
-    /// Please describe the main features of this App
+    /// This application can convert distances like:
+    /// metres to miles, miles to feet and feet to metres
     /// </summary>
     /// <author>
-    /// Student Name version 0.1
+    /// Chris Edgley
     /// </author>
     public class DistanceConverter
     {
+        // This is setting up editable variables that the
+        // user can type in 
         private double miles;
         private double feet;
         private double metres;
 
-
+        // This runs when the program starts, it outputs 
+        // a heading and allows the user to select an option
         public void ConvertDistance()
         {
             OutputHeading();
             InputOptions();
         }
 
+        // This method allows a user to select which
+        // distance to convert from and to
+        
         public void InputOptions()
         {
             Console.Write("Please state what you wish to convert: ");
@@ -42,6 +49,9 @@ namespace ConsoleAppProject.App01
             }
         }
 
+        // This method allows a user to input metres and calculate
+        // what that would be in miles, then showing it to the user
+
         private void InputMetres()
         {
             Console.WriteLine("So you have chosen Metres to Miles");
@@ -53,6 +63,9 @@ namespace ConsoleAppProject.App01
 
         }
 
+        // This method allows a user to input miles and calculate
+        // what that would be in feet, then showing it to the user
+
         private void InputMiles()
         {
             Console.WriteLine("So you have chosen Miles to Feet");
@@ -63,47 +76,53 @@ namespace ConsoleAppProject.App01
             OutputFeet();
         }
 
+        // This method allows a user to input feet and calculate
+        // what that would be in metres, then showing it to the user
+
         private void InputFeet()
         {
             Console.WriteLine("So you have chosen Feet to Metres ");
-            Console.WriteLine("Please enter the number of miles: ");
+            Console.WriteLine("Please enter the number of feet: ");
             string input = Console.ReadLine();
             feet = Convert.ToDouble(input);
             CalculateMetres();
             OutputMetres();
         }
-
+        // This method allows the computer to "convert" miles to feet
         private void CalculateFeet()
         {
             feet = miles * 5280;
         }
-
+        // This method allows the computer to "convert" feet to metres
         private void CalculateMetres()
         {
             metres = feet / 3.241;
         }
-
+        // This method allows the computer to "convert" metres to miles
         private void CalculateMiles()
         {
             miles = metres / 1609;
         }
 
-
+        // this method outputs the calculation of miles to feet
         private void OutputFeet()
         {
             Console.WriteLine(miles + " miles is " + feet + " feet!");
         }
-
+        // this method outputs the calculation of feet to metres
         private void OutputMetres()
         {
             Console.WriteLine(feet + " feet is " + metres + " metres!");
         }
-
+        // this method outputs the calculation of metres to miles
         private void OutputMiles()
         {
             Console.WriteLine(metres + " metres is " + miles + " miles!");
 
         }
+        // this method outputs the heading for the user  to see when they load
+        // the program
+
         private void OutputHeading()
         {
             Console.WriteLine("\n--------------------------------");
